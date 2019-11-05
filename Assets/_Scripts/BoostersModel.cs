2944ca03e6abd9859c927c17f460a588b03d441a
Nikita <combatwombat@ro.ru>
2019-11-05 22:14:49 +0700

    Added boosters

---
+++ Assets/_Scripts/BoostersModel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BoostersModel : MonoBehaviour
+{
+    //TODO Disable controller if booster is not available
+    public int RefreshesCount { get; set; }
+    public int AddsCount { get; set; }
+    public int ClearsCount { get; set; }
+
+    void Start()
+    {
+        RefreshesCount = 1;
+        AddsCount = 1;
+        ClearsCount = 1;
+    }
+}
